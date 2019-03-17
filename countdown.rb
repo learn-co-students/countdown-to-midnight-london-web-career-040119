e4495d5d5d34dbb2ca58825f7883d3621474c6fb
Leah Boyd <leahboyd08@gmail.com>
2019-03-17 18:31:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+
+def countdown(seconds_to_midnight)
+  seconds_to_midnight = 10
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(seconds_to_midnight)
+  seconds_to_midnight = 10
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end  
+  return "HAPPY NEW YEAR!"
 end
+
