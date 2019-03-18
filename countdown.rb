7a4cdcac2693abb74e31972c44ac529526aebede
Nahit-1 <nahit86@outlook.com>
2019-03-18 14:29:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(seconds_midnight)
+  while seconds_midnight > 0
+    puts "#{seconds_midnight} SECOND(S)!"
+    seconds_midnight -= 1
+  end
+  return "HAPPY NEW YEAR!"
+
+end
+
+
+def countdown_with_sleep(seconds_midnight)
+  while seconds_midnight > 0
+    puts "#{seconds_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_midnight -= 1
+  end
+  return "HAPPY NEW YEAR!"
+
 end
